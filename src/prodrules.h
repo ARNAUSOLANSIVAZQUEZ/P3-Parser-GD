9c0ee81ff0eb28629c161072442d525fc52d9348
ARNAUSOLANSIVAZQUEZ <arnau.solans01@estudiant.upf.edu>
2024-02-28 09:58:06 +0100

    Production rules as doc

---
+++ src/prodrules.h
@@ -0,0 +1,15 @@
+
+#ifndef P3_PARSER_GD_PRODRULES_H
+#define P3_PARSER_GD_PRODRULES_H
+
+
+#define L1R1 "<e,s>"
+#define L1R2 "<e+t,e>"
+#define L1R3 "<t,e>"
+#define L1R4 "<t*f,t>"
+#define L1R5 "<f,t>"
+#define L1R6 "<(e),f>"
+#define L1R7 "<NUM,f>"
+
+
+#endif //P3_PARSER_GD_PRODRULES_H
