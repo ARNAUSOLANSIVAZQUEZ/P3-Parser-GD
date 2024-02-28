1474dec61c98e0933779affc0bc46644b5426217
MAranich <114926980+MAranich@users.noreply.github.com>
2024-02-28 09:26:02 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,9 +5,21 @@
 
 int main(int argc, char** argv) {
 
+    if(argc != 1) {
+        printf("Usage: ./parser <input_file.txt>\n"); 
+        return 1; 
+    }
+
+    FILE* input_file = fopen(argv[0], "rb"); 
+
+    if(input_file == NULL) {
+        printf("Cannot open file. \n"); 
+        return 2; 
+    }
 
 
 
+    fclose(input_file); 
     return 0; 
 }
 
